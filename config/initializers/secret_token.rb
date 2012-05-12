17c3de2c25a165d4d7f17067e414a0aafc486352
Darin King <bandstalkonline@gmail.com>
2012-05-12 15:43:58 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '96b60058ad21b84eb80b03939bfc0e767eb0a66380b1d502f5570ff8991c4754d2ffe1f6434ad7a95e652420de568f4a5bba77a347a4b4c25f1c66c3bafe7184'
